5f8fbcccee2b0ccc29ff28675d8dccc287a0aedc
Gail Hobbs <gbhobbs97@gmail.com>
2023-03-23 09:54:13 -0700

    completed

--- lib/classes/movie.py
+++ lib/classes/movie.py
@@ -1,13 +1,38 @@
+import statistics
+
 class Movie:
-    
+    all = []
     def __init__(self, title):
-        pass
+        if (type(title) == str and len(title) > 0):
+            self._title = title
+            self.reviews = []
+            self.reviewers = []
+            Movie.all.append(self)
+        else:
+            raise Exception("title must be > 0")
+
+    def get_title(self):
+        return self._title
+    
+    def set_title(self, title):
+        if (type(title) == str and len(title) > 0):
+            self._title = title
 
     # title property goes here!
+    title = property(get_title, set_title)
 
     def average_rating(self):
-        pass
+        return statistics.mean([review.get_rating() for review in self.reviews])
 
     @classmethod
     def highest_rated(cls):
-        pass
+        countobject = {}
+    
+        for movie in cls.all:
+            countobject[movie] = movie.average_rating()
+
+        return max(countobject, key = countobject.get)
+
+
+    def add_review(self, review):
+        self.reviews.append(review)
--- lib/classes/review.py
+++ lib/classes/review.py
@@ -1,9 +1,38 @@
 class Review:
     
-    def __init__(self, viewer, movie, rating):
-        pass
+   def __init__(self, viewer, movie, rating):
+        from .viewer import Viewer
+        from .movie import Movie
+        if (isinstance(viewer, Viewer)):
+           self.viewer = viewer
+        else:
+           raise Exception('must be Viewer')
+           
+        if (isinstance(movie, Movie)):
+           self.movie = movie
+        else:
+           raise Exception('must be Movie')
+        if (type(rating) == int and 1 <= rating <= 5):
+           self._rating = rating
+        else:
+           raise Exception('must be integer from 1 to 5')
+        
+        movie.add_review(self)
+        viewer.add_review(self)
+        viewer.add_movie(movie)
+        
+
+   
+   def get_rating(self):
+      return self._rating
+   
+   def set_rating(self, rating):
+      if (type(rating) == int and 1 <= rating <= 5):
+         self._rating = rating
+    
 
     # rating property goes here!
+   rating = property(get_rating, set_rating)
 
     # viewer property goes here!
 
